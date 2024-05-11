a76d744bd0351890bd748c78e57159771c326827
Amit Gupta <amitg4@amdocs.com>
2024-05-11 12:48:30 +0530

    my first commit to awesome AI - Interviewer

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Navigation />
+        <slot />
+    </div>
+</template>
+
+
+<script setup>
+import Navigation from "./navigation.vue";
+// defineNuxtComponent({
+//     Navigation
+// })
+
+</script>
