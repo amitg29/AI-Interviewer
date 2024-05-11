3f6b3bdf29d669fd58266c2b9b545fa1dc7b7225
Amit Gupta <amitg4@amdocs.com>
2024-05-11 13:25:19 +0530

    refactor: making code module wise for better maintenance

---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Hi , Interviewer
+    </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+    title: 'Interview - AI',
+    ogTitle: 'My Amazing website for interviews',
+    description: 'This is my amazing site, which can take your interview for any exams / jobs',
+    ogDescription: 'This is my amazing site, which can take your interview for any exams / jobs.',
+    ogImage: 'https://example.com/image.png',
+    twitterCard: 'summary_large_image',
+})
+</script>
